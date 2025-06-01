fb7e142d84d70f045a67aa2ca6472b2404d01265
yichenliu <583076221@qq.com>
2025-06-02 04:38:30 +0800

    change host

--- main.py
+++ main.py
@@ -9,7 +9,7 @@
 from liveMan import DouyinLiveWebFetcher
 
 if __name__ == '__main__':
-    live_id = '890282099587'
+    live_id = '598969459734'
     room = DouyinLiveWebFetcher(live_id)
     room.get_room_status()
     room.start()
